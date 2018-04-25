cda0732ae1d4e2f54c1024cf125aeac3d0671800
Aurelien <aurelien@wildcodeschool.fr>
2018-04-25 12:20:48 +0200

    Style first steps

---
+++ src/Controller/CheckpointController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 11/10/17
+ * Time: 16:07
+ * PHP version 7
+ */
+
+namespace Controller;
+
+/**
+ * Class CheckpointController
+ *
+ */
+class CheckpointController extends AbstractController
+{
+
+    /**
+     * Display checkpoint listing
+     *
+     * @return string
+     */
+    public function index()
+    {
+        return $this->twig->render('Checkpoint/index.html.twig');
+    }
+}
